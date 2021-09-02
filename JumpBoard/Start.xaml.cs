fd23963c61b0d6e6dc7f81fe98ebf7ff26399a5d
JumpOff7667 <hackingontop@gmail.com>
2021-09-02 18:36:47 +0200

    Created Start Page

---
+++ JumpBoard/Start.xaml.cs
@@ -0,0 +1,40 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JumpBoard
+{
+    /// <summary>
+    /// Interaction logic for Start.xaml
+    /// </summary>
+    public partial class Start : Page
+    {
+        public Start()
+        {
+            InitializeComponent();
+            EnumerateAudioOut();
+        }
+
+        private void EnumerateAudioOut()
+        {
+            for (int n = -1; n < WaveOut.DeviceCount; n++)
+            {
+                WaveOutCapabilities caps = WaveOut.GetCapabilities(n);
+                FirstOutputComboBox.Items.Add(caps.ProductName); // Substract 2 from ComboBox selection index to get device number
+                SecondOutputComboBox.Items.Add(caps.ProductName);
+            }
+        }
+    }
+}
